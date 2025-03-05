8d4183334f0aa4800229d1111abdd86fe4bd6006
dendron6 <denyskhomenko@yahoo.com>
2025-03-04 23:54:01 -0500

    small changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
-import path from 'path';
+// import path from 'path';
 // const authFileGit = path.join(__dirname, '../playwright/.auth/userGit.json');
 
 
 
-test('test sauce deom', async ({ page }) => {
+test('sauce deom', async ({ page }) => {
   // test.use({ storageState: authFileGit });
   await page.goto(process.env.SAUCE_DEMO_URL+'/inventory.html');
   await page.screenshot({ path: './screenshots/sauce.png' });
   await expect(page).toHaveTitle(/Swag Labs/);
 });
 
-test('test cart', async ({ page }) => {
+test('cart', async ({ page }) => {
   // test.use({ storageState: authFileGit });
   await page.goto(process.env.SAUCE_DEMO_URL+'/inventory.html');
   await page.locator('[data-test="shopping-cart-link"]').click();
